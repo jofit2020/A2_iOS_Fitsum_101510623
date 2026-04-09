5e1a8048c85e788e919ec74b777bfae14ee689c2
Jofit <189800769+jofit2020@users.noreply.github.com>
2026-04-09 18:19:23 +0100

    Initial Commit

---
+++ A2_iOS_Fitsum_101510623/A2_iOS_Fitsum_101510623App.swift
@@ -0,0 +1,21 @@
+//
+//  A2_iOS_Fitsum_101510623App.swift
+//  A2_iOS_Fitsum_101510623
+//
+//  Created by Serbijos on 09/04/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct A2_iOS_Fitsum_101510623App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
